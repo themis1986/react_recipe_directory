9e6a43ee56bc0deed485cca56d0e97b70f033f82
thiakovidis <thiakovidis@agroapps.gr>
2022-11-22 16:57:18 +0200

    Add Context API, create context & provider, create custom context hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useTheme } from "../hooks/useTheme";
 // *components
 import Searchbar from "./Searchbar";
 // *styles
@@ -7,8 +8,10 @@ import "./Navbar.css";
 import React from "react";
 
 export default function Navbar() {
+  const { color } = useTheme();
+
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Cooking Ninja</h1>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "./context/ThemeContext";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
