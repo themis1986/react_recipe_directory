f26ac004319739bb06174273f8e8a91b987e186f
thiakovidis <thiakovidis@agroapps.gr>
2022-11-23 12:39:25 +0200

    Fetch recipes collection data from firestore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,5 @@
 import firebase from "firebase/app";
-import "firebase/firesotre";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDZC6pHr7wcdJ4GealeE6lD9bbTLjE7bcg",
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,18 +1,47 @@
-import React from "react";
-import { useFetch } from "../../hooks/useFetch";
+import React, { useEffect, useState } from "react";
+import { projectFirestore } from "../../firebase/config";
 // *components
 import RecipeList from "../../components/RecipeList";
 // *styles
 import "./Home.css";
 
 export default function Home() {
-  const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setIsPending(true);
+
+    projectFirestore
+      .collection("recipes")
+      .get()
+      .then((snapshot) => {
+        // console.log(snapshot);
+        if (snapshot.empty) {
+          setError("No recipes to load");
+          setIsPending(false);
+        } else {
+          let results = [];
+          snapshot.docs.forEach((doc) => {
+            console.log(doc);
+            results.push({ ...doc.data(), id: doc.id });
+          });
+          setData(results);
+          setIsPending(false);
+        }
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsPending(false);
+      });
+  }, []);
 
   return (
     <div className="home">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {data && <RecipeList recipes={data}/>}
+      {data && <RecipeList recipes={data} />}
     </div>
   );
 }
